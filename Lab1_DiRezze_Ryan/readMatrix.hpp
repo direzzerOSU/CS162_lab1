5eac2939454fe09e0a32b2c12f635162a94bb7c7
Ryan DiRezze <direzzer@oregonstate.edu>
2018-10-23 16:48:34 -0700

    submitted

---
+++ Lab1_DiRezze_Ryan/readMatrix.hpp
@@ -0,0 +1,17 @@
+/******************************
+** Program Name: Lab 1
+** Author: Ryan DiRezze
+** Date: September 30, 2018
+** Description: Header file for the readMatrix function and its implementation file, including
+   printMatrix, which is also defined/implemented within readMatrix.cpp.
+******************************/
+
+#ifndef READMATRIX_H
+#define READMATRIX_H
+
+// function prototypes
+void readMatrix(int** matrix, int size);
+void printMatrix(int** matrix, int size);
+bool isInteger();
+
+#endif
